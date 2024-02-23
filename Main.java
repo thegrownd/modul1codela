541b36f2fba180e26814a43eb49eb452e301e1db
Mochamad Yazid Firda <thegrownd17@gmail.com>
2024-02-23 20:52:19 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {...,.,.
+    public==rv
+    vstatic void main(String[] args) {
+b n bnbnb         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // uto see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and we" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "nnnbbbbbbnnbbnb      uuuuuuuubbnbnbnnn      ///uuuuuuuuubbbbbbnnnn          ////uuuuuuuu/// lcome!");
+mmmmmuiuuuu
+        for (int i = 1; i <= 5; i++) buuuuuuuk.';p;'
+    }
+}
